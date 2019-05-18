f3510d1ca6b16790900bc621d5d12ae3a3e7c864
TristanHoladay <tholaday777@gmail.com>
2019-05-17 20:54:29 -0600

    Game working, but without Tie Method and player switching

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -20,80 +20,99 @@ namespace TicTacToe
 
         static void Main(string[] args)
         {
-            do
-            {
-                printBoard();
-                getInput(); 
-            } while (!hasWon() && !isTie());
-
-            Console.ReadLine();
+            printBoard();
+            getInput();
         }
 
         #region Input
         public static void getInput()
         {
-            Console.WriteLine("Player " + currentPlayer);
-            Console.WriteLine("Input a row: ");
-            int row = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input a column: ");
-            int column = int.Parse(Console.ReadLine());
+            
+            do
+            {
+                Console.WriteLine("Player " + currentPlayer);
+                Console.WriteLine("Input a row and press enter: ");
+                Console.ReadLine();
+                int row = int.Parse(Console.ReadLine());
+                Console.WriteLine("You typed " + row);
+                Console.WriteLine("Input a column and press enter: ");
+                int column = int.Parse(Console.ReadLine());
+                Console.WriteLine("You typed " + column);
+                Console.ReadLine();
+                placeMark(row, column);
 
-            placeMark(row, column);
+            } while (!hasWon() && !isTie());
+
+            Console.ReadLine();
         }
         #endregion
 
         #region placemark
         static void placeMark(int row, int col)
         {
-            int rowIndx = row - 1;
-            int colIndx = col - 1;
+            int rowIndx = (row - 1);
+            int colIndx = (col - 1);
           
            board[rowIndx, colIndx] = currentPlayer;
+           Console.WriteLine(board);
             
             printBoard();
-            hasWon();
         }
         #endregion
 
-
+        #region winning methods
         static bool isHorizontalWin()
         {
+
             for(int i = 0; i <3; i++)
             {
-                if (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2])
+                if (board[i, 0] == currentPlayer && (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2]))
                 {
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
             }
 
+                return false;
+            
         }
         
         static bool isVerticalWin()
         {
+          
+
             for(int j = 0; j<3; j++)
             {
-                if(board[0, j] == board[1, j] && board[0, j] == board[2, j])
+                if(board[0, j] == currentPlayer && (board[0, j] == board[1, j] && board[0, j] == board[2, j]))
                 {
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
             }
+
+           
+                return false;
+            
         }
 
         static bool isDiagonalWin()
         {
-            for(int i = 0; i<3; i++)
+            
+            if(board[1, 1] == currentPlayer && ((board[0,0] == board[1,1] && board[0,0] == board[0,2]) || (board[2,0] == board[1,1] && board[2,0] == board[0,2])))
             {
-
+                return true;
             }
+            
+                return false;
+            
         }
+        
+
+        static bool isTie()
+        {
+            //if spaces are empty return false--it's not a tie
+            return false;
+            
+        }
+        #endregion
 
         #region win? method
         static bool hasWon()
@@ -101,22 +120,12 @@ namespace TicTacToe
             if(isHorizontalWin() || isVerticalWin() || isDiagonalWin())
             {
                 return true;
-            }
-            else if (isTie())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            };
+
+
+            return false;
         }
         #endregion
-
-        static bool isTie()
-        {
-            
-        }
 
         #region Print the game Board
         static void printBoard()
@@ -133,7 +142,6 @@ namespace TicTacToe
             }
             Console.WriteLine("-------");
             Console.Read();
-
         }
 
     }
