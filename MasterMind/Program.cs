9e7ac57ab5a4f55b8bd47150e788e96269c54e94
TristanHoladay <tholaday777@gmail.com>
2019-05-21 19:14:27 -0600

    MasterMind working.

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -9,29 +9,80 @@ namespace MasterMind
     class Program
     {
 
+        static string[] secret = new string[2];
+        static string[] colorArray = new string[] { "red", "green", "blue" };
+        static bool gameOver = false;
+        
+
         static void Main(string[] args)
         {
-            colorPicker();
+            createSecrete();
+            userGuess();
+            Console.Read();
         }
 
-        //generate randome colors and hold in variable
-        static void colorPicker()
+        //Generate Random Secret Values
+        public static void createSecrete()
         {
-            string[] colors = { "Red", "Blue", "Green" };
-            Random numbers = new Random();
+            for (int i = 0; i < 2; i++)
+            {
+                Random num = new Random();
+                secret[i] = colorArray[num.Next(0, 3)];
+            }
+        }
 
-            int colorIndx1 = numbers.Next(0, 3);
-            int colorIndx2 = numbers.Next(0, 3);
-            Console.WriteLine(colors[colorIndx1] + " " + colors[colorIndx2]);
-            Console.Read();
+        //Get Guess from User and Compare to Secret
+        public static void userGuess()
+        {
+
+            Console.WriteLine("I'm thinking of 2 colors. Try and guess.");
+            while (gameOver == false)
+            {
+
+                Console.WriteLine("Enter your guess: ");
+                string[] guess = Console.ReadLine().Split(' ');
+                string guess0 = guess[0].ToLower();
+                string guess1 = guess[1].ToLower();
+
+
+                if (guess.Length > 2)
+                {
+                   Console.WriteLine("That's too many colors. \nTry again: ");
+                   guess = Console.ReadLine().Split(' ');
+                }
+
+                //Check if Guess is Correct
+                if(secret[0] == guess0 && secret[1] == guess1)
+                {
+                    Console.WriteLine("That's Correct! You Won!");
+                    gameOver = true;
+                }
+                else
+                {
+                    int colorHintCount = 0;
+                    int positionHintCount = 0;
+
+                    //Any Colors Match
+                    for(int i=0; i<2; i++)
+                    {
+                        if (secret[i] == guess[i].ToLower())
+                        {
+                            colorHintCount++;
+                        }
+                    }
+
+                    //Any Positions Match
+                    for(int i = 0; i < 2; i++)
+                    {
+                        if (secret[i] == guess[i].ToLower())
+                        {
+                            positionHintCount++;
+                        }
+                    }
+
+                    Console.WriteLine(colorHintCount + "-" + positionHintCount);
+                }
+            }
         }
     }
 }
-
-
-
-//1. Generate Code
-
-//2. Ask User Input
-//3. If not correct answer give hint
-//4. call within while loop
