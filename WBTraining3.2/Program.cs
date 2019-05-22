3b7ee5cba8886eabc01446dbbc5e97f08efd18ca
TristanHoladay <tholaday777@gmail.com>
2019-05-22 17:51:58 -0600

    White Board Training Week 3 day 2

---
+++ WBTraining3.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBTraining3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the longest string.");
+
+            Console.WriteLine("Enter a sentence and I will find the longest word in it:");
+
+            string userSentence = Console.ReadLine().Trim();
+
+            string[] userWords = userSentence.Split(' ');
+            char[] punctuation = new char[] { ',', '.', '?', '!', '/', '\\', '*', '$', '%'};
+
+            string longestWord = "";
+            string tempWord = "";
+            int wordLength = 0;
+
+            foreach(string word in userWords)
+            {
+                for(int i=0; i<punctuation.Length; i++)
+                {
+                    tempWord = word.Replace(punctuation[i].ToString(), "");
+                }
+               
+            }
+
+            if(tempWord.Length > longestWord.Length)
+            {
+                longestWord = tempWord;
+            }
+            wordLength = longestWord.Length;
+
+            Console.WriteLine("The longest word is {0}. It has a length of {1}.", longestWord, wordLength);
+            Console.Read();
+        }
+    }
+}
