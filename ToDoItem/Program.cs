a3d7d5da69c1a058d95105ab6e6a6fb18a8abb03
TristanHoladay <tholaday777@gmail.com>
2019-06-03 19:55:28 -0600

    ToDoItem First Draft

---
+++ ToDoItem/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ToDoItem
+{
+    #region ToDoItem Class
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public string Priority { get; set; }
+
+        public ToDoItem(string Des, string Date, string Prior)
+        {
+            Description = Des;
+            DueDate = Date;
+            Priority = Prior;
+        }
+
+    }
+    #endregion
+    class Program
+    {
+        private static List<ToDoItem> ToDoList = new List<ToDoItem>();
+
+        static void Main(string[] args)
+        {
+            CreateItems();
+            OutPut();
+            Console.Read();
+        }
+
+        #region Add Items
+        public static void CreateItems()
+        {
+            Console.WriteLine("Welcome to your To Do List");
+            Console.WriteLine("Would you like to add an item or show your current list?");
+            string Choice = Console.ReadLine().ToLower();
+
+            while (Choice != "show")
+            {
+                Console.Write("Please Enter a Description: ");
+                string description = Console.ReadLine();
+
+                Console.Write("Please Enter a Due Date in the format MM/DD/YYYY: ");
+                string date = Console.ReadLine();
+
+                Console.Write("Please Enter the Item's Priority Level (High, Normal, or Low): ");
+                string priority = Console.ReadLine();
+
+                ToDoItem item = new ToDoItem(description, date, priority);
+                ToDoList.Add(item);
+
+                Console.WriteLine("Add Another Item? Yes/No");
+                string secondChoice = Console.ReadLine().ToLower();
+                if(secondChoice == "no")
+                {
+                    break;
+                }
+
+            }
+        }
+        #endregion
+
+        public static void OutPut()
+        {
+            int count = 0;
+
+            foreach(ToDoItem item in ToDoList)
+            {
+                count += 1;
+                Console.WriteLine("Things to Do:");
+                Console.WriteLine(count+ ") " + item.Description + "| " + item.DueDate + "| " + item.Priority);
+            }
+            
+        }
+    }
+}
---
+++ WhiteBoard4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteBoard4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please give 5 numbers separated by commas: ");
+
+            string[] Array = Console.ReadLine().Split(',', ' ');
+
+            for(int i=Array.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(Array[i]);
+            }
+
+            Console.Read(); 
+
+
+
+        }
+    }
+}
